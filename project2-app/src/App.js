ac0346f0334362579e6956eed24ec92373d124ed
Emma Blaido <emmablaido@Emmas-Air.lan>
2022-12-06 13:00:25 -0600

    test two

--- project2-app/src/App.js
+++ project2-app/src/App.js
@@ -4,50 +4,15 @@ import {useState, useEffect} from 'react';
 import {useParams} from 'react-router'
 import Drinks from './VodkaDrinks';
 import { Routes, Route } from 'react-router-dom'
+import Mainpage from './components/MainPage';
 
 function App() {
-  	
-  	  const id = "b"
-  	    // console.log(id)
-  	  const url= "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=vodka"
-  	  const [drink, setDrink]=useState(null)
-  	  useEffect( ()=> {
-  	      fetch(url)
-  	          .then((response)=>response.json())
-  	          .then((json)=> {
-  	              setDrink(json)
-  	              console.log(drink.drinks)
-  	      })
-  	          .catch(console.error)
-  
-  	  }, [])
-  	  if (drink === null){
-  	  return ( 
-  	    <h1>Loading..</h1>)
-  	  }else{
-  
-      return(
-    <div className="App">
-      <h1>Drink:</h1>
-      {drink.drinks.map((drinky, index)=>{
-        return(
-          <div key={index}>
-          <h1>{drinky.strDrink}</h1>
-          <img src={drinky.strDrinkThumb}height="200"/>
-          </div>
-
-        )
-      })
-      }
-      <main>
-        {/* <Routes>
-          <Route path="/" element={ <Drinks /> } />
-        </Routes> */}
-      </main>
-      
-
+  	return (
+  	 <div>
+        <Mainpage/>
     </div>
-     )}}
-
+    )
+     }
+console.log(Mainpage)
 
 export default App;
--- project2-app/src/components/MainPage.js
+++ project2-app/src/components/MainPage.js
@@ -1,4 +1,17 @@
+import React from "react";
+
 function Mainpage(props) {
-    <h1>im working</h1>
-    return 
+    return<>
+    <h1>Clink</h1>
+    <button id="Vodka">Vodka</button>
+    <button id="Rum">Rum</button>
+    <button id="Whiskey">Whiskey</button>
+    <button id="Gin">Gin</button>
+
+    
+    
+    
+    </>
+    
 }
+export default Mainpage;
