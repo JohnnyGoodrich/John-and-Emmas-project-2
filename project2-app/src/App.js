2f1dcd537e540b807e3f48eb0b1c0a1678850a79
JohnnyGoodrich <jbgoodrich26@gmail.com>
2022-12-06 14:57:32 -0500

    made the drink icons links

--- project2-app/src/App.js
+++ project2-app/src/App.js
@@ -5,6 +5,7 @@ import {useParams} from 'react-router'
 import Drinks from './Drinks';
 import { Routes, Route } from 'react-router-dom'
 import Mainpage from './components/MainPage';
+import DrinkDetails from './DrinkDetails';
 
 function App() {
   	
@@ -14,6 +15,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/drinks" element={ <Drinks /> } />
+          <Route path="/drinks/:url" element={ <DrinkDetails /> } />
           
         </Routes>
         <Mainpage/>
