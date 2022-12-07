cc2b06853b0694b72b0e4726319bc2caa0eb8657
Emma Blaido <emmablaido@Emmas-Air.lan>
2022-12-07 09:45:52 -0600

    background img css work

--- project2-app/src/components/MainPage.js
+++ project2-app/src/components/MainPage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Mainpage(props) {
     return<>
     <h1 id="appname">Clink</h1>
-    <img className="img-responsive" src={"https://i.pinimg.com/564x/f3/56/04/f356048cad4abaedad5000a7e2b39a92--outdoor-patio-string-lights-outdoor-patios.jpg"} alt="logo"/>
+    <img id="Mainimg" src={"https://i.pinimg.com/564x/f3/56/04/f356048cad4abaedad5000a7e2b39a92--outdoor-patio-string-lights-outdoor-patios.jpg"} alt="logo"/>
     
     
       <div>
