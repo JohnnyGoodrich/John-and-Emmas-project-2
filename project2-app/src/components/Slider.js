8d1550ca1c1b61b56bc9b42c046af76ccdc7337a
Emma Blaido <emmablaido@Emmas-Air.lan>
2022-12-09 15:13:10 -0600

    SLIDER IS WORKING

--- project2-app/src/components/Slider.js
+++ project2-app/src/components/Slider.js
@@ -1,46 +1,60 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import { Carousel } from 'react-bootstrap'
 
-function Slider(){
-    
-    const url= `https://www.thecocktaildb.com/api/json/v1/1/random.php`
-    const [drinksD, setDrinksD]=useState(null)
-    useEffect( ()=> {
-        fetch(url)
-            .then((response)=>response.json())
-            .then((json)=> {
-                setDrinksD(json)  
-                    
-        })
-            .catch(console.error)
-    }, [])
-    return (
-        
-        drinksD ?
-        
-        <div className="details-container">
-            <section className='drink-ing'>
-                <div className='drink-title-pic'>
-                    <h2>{drinksD.drinks[0].strDrink}</h2>
-                    <img src={drinksD.drinks[0].strDrinkThumb}height="300"/>
-                    
-                </div>
-            </section>
-        </div> :<p>Loading...</p>
-        
-        ); 
 
-}
-function Interval (props){
+function Slider() {
+
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`
+
+
+    const [drinks, setDrinks] = useState(null)
 
     useEffect(() => {
-        let interval = setInterval(async () => {
-            const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`);
-        }, 2000);
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
-    }
+        fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+                setDrinks(json)
+
+            })
+            .catch(console.error)
+    }, [])
+
+    const [index, setIndex] = useState(0)
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+        console.log(index)
+    };
+    if (!drinks) {
+        return (<h1>no drinks</h1>)
+    }else{
+       console.log(drinks.drinks)
+        const eachDrink = drinks.drinks.map((drink, imageIndex) => 
+        <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={drinks.drinks[imageIndex].strDrinkThumb}
+          alt="drink"/>
+          <Carousel.Caption>
+          <h3>{drinks.drinks[imageIndex].strDrink
+}</h3>
+          <p>{drinks.drinks[imageIndex].strCategory
+}</p>
+        </Carousel.Caption>
+        </Carousel.Item>
+        )
+        
+        return (
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+    {eachDrink}
+</Carousel>
+
+
+
+        );
+
+}
+}
+
 
 export default Slider;
